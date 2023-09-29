18c496c7174403044cac2e92dab4e5e70045c2c2
Семён Иванов <lilepopendr1@mail.ru>
2023-09-29 16:34:02 +0500

    Task6

---
+++ Tyuiu.IvanovSI.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovSI.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovSI.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d27094ee-4fb6-4dcf-b463-0568e401c6ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.IvanovSI.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IvanovSI.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.IvanovSI.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] narraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов Массива = " + DataService.AdditionArray(narraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(narraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(narraynums));
+            Console.ReadKey();
+        }
+    }
+}
